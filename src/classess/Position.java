a8016c64357bf5496a52d0ab2e630a28e37dee3d
lysa-thorn <lysa.thorn@gmail.com>
2021-04-28 17:18:11 +0700

    List Position

--- src/classess/Position.java
+++ src/classess/Position.java
@@ -1,12 +1,38 @@
 package classess;
 
+import javafx.scene.control.Button;
+
 public class Position {
-	String name;
-	public Position(String name){
-		this.name = name;
+	
+	public String id;
+	public String position;
+
+	public Position(String id,String position){
+		this.id = id;
+		this.position = position;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
 	}
 	
+	@Override
 	public String toString() {
-		return "Positions:" + name;
+		return "Position [id=" + id + ", position=" + position +"]";
 	}
+	
+	
+	
 }
